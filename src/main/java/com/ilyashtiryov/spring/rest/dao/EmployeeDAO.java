7cbd79c80f0d724515a6a123f5d3464c9a905c7c
Ilya <dj-nfb@mail.ru>
2023-11-02 20:16:46 +0300

    showAllEmployees

---
+++ src/main/java/com/ilyashtiryov/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.ilyashtiryov.spring.rest.dao;
+
+import com.ilyashtiryov.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
